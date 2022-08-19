8b40a8a78f201f94f7336a79e98955762f8e360d
Jesse Maraya <jessemaraya2013@gmail.com>
2022-08-19 12:22:32 -0700

    mini quiz working

--- quiz/src/App.js
+++ quiz/src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	const questions = [
+		{
+			questionText: 'What monster is this?',
+      image: 'src/images/aknosom.png',
+			answerOptions: [
+				{ answerText: 'Azuros', isCorrect: false },
+				{ answerText: 'Anjanath', isCorrect: false },
+				{ answerText: 'Aknosom', isCorrect: true },
+				{ answerText: 'Astalos', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'What monster is this?',
+      image: 'src/images/barioth.png',
+			answerOptions: [
+				{ answerText: 'Astablos', isCorrect: false },
+				{ answerText: 'Barioth', isCorrect: true },
+				{ answerText: 'Barroth', isCorrect: false },
+				{ answerText: 'Volvidon', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'What monster is this',
+      image: 'src/images/diablos.png',
+			answerOptions: [
+				{ answerText: 'Diablos', isCorrect: true },
+				{ answerText: 'Apex Diablos', isCorrect: false },
+				{ answerText: 'Espinas', isCorrect: false },
+				{ answerText: 'Garangolem', isCorrect: false },
+			],
+		},
+	];
+
+	const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [showScore, setShowScore] = useState(false);
+	const [score, setScore] = useState(0);
+
+	const handleAnswerOptionClick = (isCorrect) => {
+		if (isCorrect) {
+			setScore(score + 1);
+		}
+
+		const nextQuestion = currentQuestion + 1;
+		if (nextQuestion < questions.length) {
+			setCurrentQuestion(nextQuestion);
+		} else {
+			setShowScore(true);
+		}
+	};
+	return (
+		<div className='app'>
+			{showScore ? (
+				<div className='score-section'>
+					You scored {score} out of {questions.length}
+				</div>
+			) : (
+				<>
+					<div className='question-section'>
+						<div className='question-count'>
+							<span>Question {currentQuestion + 1}</span>/{questions.length}
+						</div>
+						<div className='question-text'>{questions[currentQuestion].questionText}</div>
+            <img className='image'></img>
+					</div>
+					<div className='answer-section'>
+						{questions[currentQuestion].answerOptions.map((answerOption) => (
+							<button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+						))}
+					</div>
+				</>
+			)}
+		</div>
+	);
 }
-
-export default App;
